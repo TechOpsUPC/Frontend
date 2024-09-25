7993b2b485fa63f0e2bf106a9761f938b85639eb
PieroMiranda1 <pieroms25@gmail.com>
2024-09-25 13:18:37 -0500

    feat: added authlayout component

---
+++ src/users/pages/auth-layout.component.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "auth-layout"
+}
+</script>
+
+<template>
+  <div class="auth-layout">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+.auth-layout {
+}
+</style>
