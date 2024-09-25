d4dbce57db8e743da90a36f7737699776dfd66c6
PieroMiranda1 <pieroms25@gmail.com>
2024-09-25 13:14:27 -0500

    feat: added toolbar component for users bounded context

---
+++ src/users/components/toolbar-content.component.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+  name: "toolbar-content",
+}
+
+</script>
+<template>
+  <pv-toolbar class="container">
+    <template #start>
+      <img alt="zen-logo" src="../../assets/film-icon-black.png" class="h-4rem">
+    </template>
+  </pv-toolbar>
+
+</template>
+
+<style scoped>
+.container{
+  background-color: #B93844;
+;
+}
+</style>
