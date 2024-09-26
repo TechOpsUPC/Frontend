e457444b619f4e1517e8849b5d3533aab0208f05
DiegoR434 <u202123548@upc.edu.pe>
2024-09-26 12:40:22 -0500

    feat(events): added event-list and event-item components and its functionality

---
+++ src/events/components/event-item.component.vue
@@ -0,0 +1,30 @@
+﻿<template>
+  <pv-card>
+    <template #title>Simple Card {{eventItem.title}}</template>
+    <template #content>
+      <p class="m-0">
+        {{eventItem.description}}
+      </p>
+    </template>
+  </pv-card>
+  equisde
+</template>
+
+<script >
+export default {
+  name: "event-item",
+  props:{
+    eventItem:{
+      type:Object,
+      required:true
+    }
+  },
+  created(){
+    console.log(this.eventItem);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
